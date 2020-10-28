2448913493cbb7765f4adc2c21079790655d4eab
Todd Demone <tdemone1@gmail.com>
2020-10-28 18:03:05 -0400

    completed to ex 10-15 regex

--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -5,5 +5,12 @@ public class MainProgram {
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        Checker checker = new Checker();
+        System.out.println(checker.isDayOfWeek("tue"));
+        System.out.println(checker.isDayOfWeek("hha"));
+        System.out.println(checker.allVowels("queue"));
+        System.out.println(checker.allVowels("oe"));
+        System.out.println(checker.timeOfDay("12:23:45"));
+        System.out.println(checker.timeOfDay("123:1:99"));
     }
 }
